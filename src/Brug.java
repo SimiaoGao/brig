92bd7d7be05faafd5f7d52e82b38b306d10f6a5b
Josh D <JoshDev23@gmail.com>
2013-12-22 04:23:01 -0500

    Added rankings + correlations

--- src/Brug.java
+++ src/Brug.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -79,19 +81,74 @@ public class Brug {
 				for( int j = 2; j < item2.length; j+=2) {
 					
 					if(i != j) {
-						newMap.put(item1[i] + "," + item2[j], item1[i+1] + "," + item2[j+1]);
+						String key = item1[i] + "," + item2[j];
+						if(newMap.containsKey(key)) {
+							newMap.put(key, newMap.get(key) + item1[i+1] + "," + item2[j+1] + ",");
+						}
+						else	{			
+						newMap.put(key, item1[i+1] + "," + item2[j+1] + ",");
+						}
 					}
 				}
 			}
 		}
 		
 		return newMap;
+	}
+	
+	
+	static HashMap<String, String> createCorrelations(HashMap<String, String> map) {
 		
+		HashMap<String, String> newMap = new HashMap<String, String>();
+		
+		double sumXY, sumX, sumY, sumXX, sumYY, n;
+		
+			
+		for (Entry<String, String> entry : map.entrySet()) {
+			sumXY = 0.0; sumX = 0.0; sumY = 0.0; sumXX = 0.0; sumYY = 0.0; n = 0.0;
+			
+			String[] s = entry.getValue().split(",");
+			
+			for( int i =0; i < s.length; i +=2) {
+				
+				double x = Double.parseDouble(s[i]);
+				double y = Double.parseDouble(s[i+1]);
+				
+				sumYY += y*y;
+				sumXX += x*x;
+				sumXY += x*y;
+				sumX += x;
+				sumY += y;
+				n++;
+			}
+			
+	
+
+			double correlation = (n*sumXY - sumX*sumY) / ( Math.sqrt(n*sumXX - (sumX*sumX))*Math.sqrt(n*sumYY - (sumY*sumY)));
+			if(( Math.sqrt(n*sumXX - (sumX*sumX))*Math.sqrt(n*sumYY - (sumY*sumY))) == 0)
+				correlation = 0;
+				newMap.put(entry.getKey(), correlation + "," + n);
+		}
+		
+		return newMap;
 	}
 	
 	
 	
-	
+	static HashMap<String, String> rankItems(HashMap<String, String> map) {
+		
+		HashMap<String, String> newMap = new HashMap<String, String>();
+		
+		for (Entry<String, String> entry : map.entrySet()) {
+			
+			String[] key = entry.getKey().split(",");
+			String[] values = entry.getValue().split(",");
+			
+			newMap.put(key[0] + "," + values[0], key[1] + "," + values[1]);
+		}
+		
+		return newMap;
+	}
 	
 	/**
 	 * @param args
@@ -104,11 +161,24 @@ public class Brug {
 		HashMap<Integer, String> groupByUserMap = groupByUser(dataFile);
 		HashMap<Integer, String> addRatingsCountMap = countRatingsPerUser(groupByUserMap);
 		HashMap<String, String> pairItemsMap = pairItems(addRatingsCountMap);
+		HashMap<String, String> correlationMap = createCorrelations(pairItemsMap);
+		HashMap<String, String> rankingMap = rankItems(correlationMap);
 		
-		for (Entry<String, String> entry : pairItemsMap.entrySet()) {
-		    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("output.txt", "UTF-8");
+			for (Entry<String, String> entry : rankingMap.entrySet()) {
+				
+			    writer.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+			}
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
-		
+
 		
 	}
 
