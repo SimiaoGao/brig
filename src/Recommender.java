69d5c0760f77610712fda631dc2904058eb1dc28
Josh D <JoshDev23@gmail.com>
2013-12-22 14:37:07 -0500

    Rwrote some parts of brug.

---
+++ src/Recommender.java
@@ -0,0 +1,137 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+
+public class Recommender {
+
+	static HashMap<Integer, String> groupUserID(String dataFile) {
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(new File(
+					dataFile)));
+
+			HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+			String nextLine;
+
+			while ((nextLine = in.readLine()) != null) {
+
+				String[] s = nextLine.split("\t");
+				int key = Integer.parseInt(s[0]);
+
+				if (map.containsKey(key)) {
+					String currList = map.get(key);
+					currList += "," + Integer.parseInt(s[1]) + ","
+							+ Integer.parseInt(s[2]);
+					map.put(key, currList);
+				} else {
+					map.put(key,
+							Integer.parseInt(s[1]) + ","
+									+ Integer.parseInt(s[2]));
+				}
+			}
+			in.close();
+
+			return map;
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found.");
+		} catch (IOException e) {
+			System.out.println("Could not read File.");
+		}
+
+		return null;
+	}
+
+	static HashMap<String, String> pairRatingsPerUser(HashMap<Integer, String> map) {
+
+		HashMap<String, String> pairs = new HashMap<String, String>();
+
+		for (Entry<Integer, String> entry : map.entrySet()) {
+
+			String[] item1 = entry.getValue().split(",");
+			String[] item2 = entry.getValue().split(",");
+
+			for (int i = 0; i < item1.length; i += 2) {
+				for (int j = 0; j < item1.length; j += 2) {
+
+					if (i != j) {
+
+						String key = item1[i] + "," + item2[j];
+
+						if (pairs.containsKey(key)) {
+							pairs.put(key, pairs.get(key) + "," + item1[i + 1]
+									+ "," + item2[j + 1]);
+						} else {
+							pairs.put(key, item1[i + 1] + "," + item2[j + 1]);
+						}
+
+					}
+				}
+			}
+
+		}
+		
+		return pairs;
+	}
+	
+	static HashMap<String, String> itemCorrelations( HashMap<String, String> map) {
+		
+		HashMap<String, String> correlations = new HashMap<String, String>();
+		
+		double sumXY, sumX, sumY, sumXX, sumYY, n;
+		
+		for (Entry<String, String> entry : map.entrySet()) {
+			sumXY = 0.0; sumX = 0.0; sumY = 0.0; sumXX = 0.0; sumYY = 0.0; n = 0.0;
+			
+			String[] s = entry.getValue().split(",");
+			
+			for( int i =0; i < s.length; i +=2) {
+				
+				double x = Double.parseDouble(s[i]);
+				double y = Double.parseDouble(s[i+1]);
+				
+				sumYY += y*y;
+				sumXX += x*x;
+				sumXY += x*y;
+				sumX += x;
+				sumY += y;
+				n++;
+			}
+			
+			if( n > 1) {
+			double correlation = (n*sumXY - sumX*sumY) / ( Math.sqrt(n*sumXX - (sumX*sumX))*Math.sqrt(n*sumYY - (sumY*sumY)));
+			if(( Math.sqrt(n*sumXX - (sumX*sumX))*Math.sqrt(n*sumYY - (sumY*sumY))) == 0)
+				correlation = 0;
+			correlations.put(entry.getKey(), correlation + "," + n);
+			}
+		
+		}
+		return correlations;
+	}
+
+	public static void main(String[] args) {
+
+		final String dataFile = "text_data/u.data";
+		final String itemFile = "text_data/u.item";
+
+		HashMap<Integer, String> userRatingMap = groupUserID(dataFile);
+		HashMap<String, String> pairs = pairRatingsPerUser(userRatingMap);
+		HashMap<String, String> correlations = itemCorrelations(pairs);
+
+		for (Entry<String, String> entry : correlations.entrySet()) {
+
+			System.out.println("Key = " + entry.getKey() + ", Value = "
+					+ entry.getValue());
+		}
+
+	}
+
+}
